516e3845a5968a9e5f2ec69a8ebb4634a314ed48
Jonas Keutel <jonas@keutel.de>
2016-01-04 20:50:22 -0800

    add Rick Astley mode; also introduce very basic version of having a conversation; also giving the answer to the question who is leading a certain place as a whole sentence'

--- index.js
+++ index.js
@@ -91,6 +91,13 @@ function onIntent(intentRequest, session, callback) {
     // Dispatch to your skill's intent handlers
     if ("WhoIsLeadingIntent" === intentName) {
         whoIsLeading(intent, session, callback);
+    } else if ("BirthdateIntent" === intentName) {
+        if (session.attributes && session.attributes.person) {
+            getBirthdate(intent, session, callback);
+        } else {
+           rickAstley(intent, session, callback); 
+        }
+        
     } else if ("HelpIntent" === intentName) {
         getWelcomeResponse(callback);
     } else {
@@ -111,12 +118,9 @@ function onSessionEnded(sessionEndedRequest, session) {
 // --------------- Functions that control the skill's behavior -----------------------
 
 function getWelcomeResponse(callback) {
-    // If we wanted to initialize the session to have some attributes we could add those here.
     var sessionAttributes = {};
     var cardTitle = "Welcome to Wikidata";
     var speechOutput = "Welcome to Wikidata. How can I help you?";
-    // If the user either does not reply to the welcome message or says something that is not
-    // understood, they will be prompted again with this text.
     var repromptText = "You can ask me when a certain person was born. " +
         "For more information please have a look at the cards at the Alexa App."; //TODO
     var shouldEndSession = false;
@@ -124,6 +128,25 @@ function getWelcomeResponse(callback) {
     callback(sessionAttributes,
         buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
 
+}
+
+function rickAstley(intent, session, callback) {
+    callback({},
+        buildSpeechletResponse("Fool you!", 
+                                "Never gonna give you up, never gonna let you down, Never gonna run around and . desert you . Never gonna make you cry, never gonna say goodbye . Never gonna tell a lie . and hurt you", 
+                                "Sorry, maybe you should try asking who is leading China.", 
+                                false)
+        );
+}
+
+function getBirthdate(intent, session, callback) {
+    name = session.attributes.person.name;
+    callback({},
+        buildSpeechletResponse("This is to do", 
+                                "I have to figure out when " + name + " was born",
+                                "", 
+                                false)
+        );
 }
 
 function whoIsLeading(intent, session, callback) {
@@ -147,23 +170,23 @@ function whoIsLeading(intent, session, callback) {
 // --------------- Custom functions -----------------------
 
 
-function getWikidataId(name, sessionAttributes, callbackQuery, callback) {
-    wikidataSearch.set('search', name);
+function getWikidataId(place, sessionAttributes, callbackQuery, callback) {
+    wikidataSearch.set('search', place);
     wikidataSearch.search(function(result, error) {
-        console.log("Results for ", name, result.results);
+        console.log("Results for ", place, result.results);
         var id = result.results[0].id;
-        callbackQuery(id, sessionAttributes, callback);
+        callbackQuery(id, place, sessionAttributes, callback);
     });
 }
 
-function doWhoIsLeadingQuery(id, sessionAttributes, callback) {
+function doWhoIsLeadingQuery(id, place, sessionAttributes, callback) {
     var query = WHO_IS_LEADING_QUERY.replace("[ITEM_ID]", id);
     client.get( SPARQL_ENDPOINT + query, function(data, response) {
         var jsonResponse = JSON.parse(decoder.write(data));
         if (jsonResponse.results.bindings.length == 0) {
             speechOutput = "Sorry, I didn't find an answer on Wikidata. Maybe its data is incomplete. " +
                             "You would do me a big favour if you could look it up and add it to Wikidata."
-            callback({},
+            callback(sessionAttributes,
                 buildSpeechletResponse("", speechOutput, "", false));
             return;
         }
@@ -174,8 +197,12 @@ function doWhoIsLeadingQuery(id, sessionAttributes, callback) {
         wikidataSearch.set('search', resultId);
         wikidataSearch.search(function(result, error) {
             var label = result.results[0].label;
-            speechOutput = label;
-            callback({},
+            sessionAttributes.person = {
+                name: label,
+                id: resultId
+            }
+            speechOutput = label + " is leading " + place;
+            callback(sessionAttributes,
                 buildSpeechletResponse("", speechOutput, "", false));
         });
     });
